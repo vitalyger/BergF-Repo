d9da6781c5a3143a8ab1934531103aef96e3d10e
vitalyger <110974931+vitalyger@users.noreply.github.com>
2023-12-07 18:42:01 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  i18n: {
+    locales: ['de', 'en'],
+    defaultLocale: 'en'
+  },
+  images: {
+    domains: ['media.graphassets.com']
+  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1
+  }
+}
